44a5e12c98a424b729b5057bf0161503f8188513
allenzeng <zengjun_cm@sina.com>
2017-02-14 15:49:55 +0800

    add document

--- src/PhotoWalls.js
+++ src/PhotoWalls.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
 
+const styles = {
+  coverBox: {
+    margin: 2,
+    position: 'relative',
+    backgroundColor: '#f0f0f0',
+    cursor: 'pointer',
+  },
+  cover: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    verticalAlign: 'bottom',
+  },
+};
+
 export default class PhotoWalls extends Component {
   render() {
     return (
-      <div>hello walls</div>
+      <div
+        style={
+          Object.assign(
+            {}, styles.coverBox,
+            { width: width * 200 / height, flexGrow: width / height * 200, opacity },
+          )}
+      >
+        <i style={{ display: 'block', paddingBottom: `${height / width * 100}%` }} />
+        <img style={styles.cover} src={`${data.url}-pcPhotographerCover`} alt="cover" />
+      </div>
     );
   }
 }
